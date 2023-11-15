3563bf8260ad064fca1f486327296c19fd0de53d
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2023-11-14 20:19:41 -0600

    4

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -15,3 +15,7 @@ let addBy1000 = addBy(1000);
 let divideBy10 = divideBy(10);
 let divideBy100 = divideBy(100);
 
+console.log(addBy100(20));
+console.log(divideBy10(20));
+console.log(divideBy100(200));
+console.log(addBy1000(20));
