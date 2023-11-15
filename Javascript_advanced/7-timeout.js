6952387ec0280cb282992c204fdfa479e030528d
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2023-11-15 12:17:46 -0600

    7

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,24 @@
+console.log("Start of the execution queue");
+
+function printNumbers() 
+
+{
+  for (let i = 1; i <= 100; i++) 
+{
+
+  
+  console.log(i);
+
+}
+}
+
+setTimeout(() => 
+
+{
+  
+printNumbers();
+  console.log("Final code block to be executed");
+}, 0);
+
+console.log("End of loop printing");
+
