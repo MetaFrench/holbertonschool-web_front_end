445214018d5e10f3681a2ee93129f042e9ad539e
Nick French <nickfrench@Nicks-MacBook-Pro.local>
2023-11-16 01:42:27 -0600

    9

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -1,11 +1,21 @@
-function countPrimeNumbers () 
+function countPrimeNumbers() {
+    let primes = [];
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+    return primes.length;
+}
 
-{
-  let count = 0;
-  for (let i = 2; i < 100; i++) 
-{
-    if (isPrime(i))
-      count++;
-}
-  return count;
-}
+const startTime = performance.now();
+const primeCount = countPrimeNumbers();
+const endTime = performance.now();
+
